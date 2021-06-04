6e50627e447a3d2699a8ed5d92bf358e7a452e52
VikhilP <vikhilp98@gmail.com>
2021-06-04 15:11:19 +0100

    service 2 app comp

--- service-2/app.py
+++ service-2/app.py
@@ -3,13 +3,11 @@ import random
 
 app = Flask(__name__)
 
-
-@app.route('/', methods= ["GET"])
-# Player Position
-def pos():
-    positions = ["QB","HB","FB","WR","T","G","C","TE",
+positions = ["QB","HB","FB","WR","T","G","C","TE",
     "CB","LB","DT","DE","SS","FS","K","KR","P"]
 
-    player_position = random.choice(positions)
-    return player_position
+@app.route('/getpositions', methods= ["GET"])
+# Player Position
+def pos():
+    return random.choice(positions)
 
