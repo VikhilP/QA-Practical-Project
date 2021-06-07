9902b5adbcf392877fc170f34072c9eb6ec5ee3f
VikhilP <vikhilp98@gmail.com>
2021-06-07 23:45:34 +0000

    service-1 testing half comp

--- service-1/testing/test_service-1.py
+++ service-1/testing/test_service-1.py
@@ -1,16 +1,17 @@
 from flask_testing import TestCase
 from flask import url_for
 import requests
-import requests_mock
+from requests_mock import mock
 from application import app
 
 class TestBase(TestCase):
     def create_app(self):
+        app.config['SECRET_KEY'] = "dfs"
         return app
 
 class TestResponse(TestBase):
     def test_index(self):
-        with requests_mock.mock() as m:
+        with mock() as m:
             m.get('http://service-2:5000/getpositions', text='QB')
             m.get('http://service-3:5000/getpickorder', json=35)
             m.post('http://service-4:5000/calculatedraftround', json={'draft_number': 35, 'draft_round': 2, 'position': 'QB', 'round_pick': 3})
@@ -22,5 +23,5 @@ class TestResponse(TestBase):
             
 
 
-            self.assertIn(a, response.data.decode())
+        self.assertIn(a, response.data.decode())
 
