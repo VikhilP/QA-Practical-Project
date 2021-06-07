2a23b2c0c07b24c45084baa00b88b00388d6d33c
VikhilP <vikhilp98@gmail.com>
2021-06-07 14:01:52 +0000

    hooked up to docker

--- service-1/application/__init__.py
+++ service-1/application/__init__.py
@@ -3,9 +3,9 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
-app.config['SQL_DATABASE_URI'] = "sqlite:///"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SECRET_KEY'] = "dfs"
-
+db = SQLAlchemy(app)
 from application import routes
--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -8,22 +8,24 @@ import random, requests
 @app.route('/home', methods = ['GET', 'POST'])
 def home():
     form = GenerateDraft()
-    if request.method == 'GET':
-        return render_template('index.html', title='Draft', form = form)
+    print(form.validate_on_submit())
+    print(form.errors)
     if form.validate_on_submit():
         draft_number = requests.get('http://service_3:5002/getpickorder').json()
         position = requests.get('http://service_2:5001/getpositions').text
 
         senditems = {"dnum": draft_number, "pos": position}
-        info = requests.post("http://service-4:5003/round", json=senditems.json())
+        info = requests.post("http://service_4:5003/round", json=senditems).json()
 
         new_rookie = draft(
-            position = info.get["position"],
-            pick = info.get["draft_number"] 
+            position = info["position"],
+            pick = info["draft_number"]
             )
         db.session.add(new_rookie)
         db.session.commit()
         
         last_picks = draft.query.order_by(desc(draft.id)).limit(5).all()
-
+        print(info)
+        print(last_picks)
         return render_template('index.html', title='Draft', form = form, info=info, last_picks = last_picks)
+    return render_template('index.html', title='Draft', form = form)
