acfe93aee700fbf12580c23a88e7923fa47f32a4
VikhilP <vikhilp98@gmail.com>
2021-06-08 15:39:26 +0000

    adding new requirements.txt

--- setupscripts.sh
+++ setupscripts.sh
@@ -17,3 +17,5 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+
+pip3 install -r requirements.txt
