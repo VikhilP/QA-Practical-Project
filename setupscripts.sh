ea184aa396ec81b1bafc5df36b9b3b8f2fc819ce
VikhilP <vikhilp98@gmail.com>
2021-06-08 16:14:14 +0000

    added remove statement

--- setupscripts.sh
+++ setupscripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt install python3-venv
+rm -rf venv
 python3 -m venv venv
 source venv/bin/activate
 python3 --version
@@ -18,6 +19,5 @@ sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
 
 pip3 install -r requirements.txt
