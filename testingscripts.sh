3bfd486bdda2dbdc12f64846774db7c1a573976f
VikhilP <vikhilp98@gmail.com>
2021-06-08 15:41:59 +0000

    adding new requirements.txt to test scripts

--- testingscripts.sh
+++ testingscripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-
+pip3 install -r requirements.txt
 python3 -m pytest service-1/testing/test_service-1.py --cov=.
 python3 -m pytest service-2/testing/test_service-2.py --cov=.
 python3 -m pytest service-3/testing/test_service-3.py --cov=.
