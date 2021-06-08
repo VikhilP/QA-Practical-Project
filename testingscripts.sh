66fe9f3067c6a3a0b9cb21082e28dd60e5b5e3b4
VikhilP <vikhilp98@gmail.com>
2021-06-08 15:53:23 +0000

    made so not everything is tested

--- testingscripts.sh
+++ testingscripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest service-1/testing/test_service-1.py --cov=.
-python3 -m pytest service-2/testing/test_service-2.py --cov=.
-python3 -m pytest service-3/testing/test_service-3.py --cov=.
-python3 -m pytest service-4/testing/test_service-4.py --cov=. 
+python3 -m pytest service-1/testing/test_service-1.py --cov=application
+python3 -m pytest service-2/testing/test_service-2.py --cov=app
+python3 -m pytest service-3/testing/test_service-3.py --cov=app
+python3 -m pytest service-4/testing/test_service-4.py --cov=app
