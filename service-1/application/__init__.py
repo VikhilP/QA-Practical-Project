d37304e48e1a6831adb0f7dcfc00e3d42fc383ae
VikhilP <vikhilp98@gmail.com>
2021-06-08 16:42:13 +0000

    testing database uri

--- service-1/application/__init__.py
+++ service-1/application/__init__.py
@@ -6,7 +6,7 @@ from os import getenv
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI") # "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] ="mysql+pymysql://root:password@34.105.162.197:3306/draft" # getenv("DATABASE_URI") # "sqlite:///data.db"
 app.config['SECRET_KEY'] = "dfs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
