34f5d75bf54d2596897a25acc8b1cdcf5580ea8e
VikhilP <vikhilp98@gmail.com>
2021-06-09 14:23:38 +0000

    debugging

--- setupscripts.sh
+++ setupscripts.sh
@@ -23,15 +23,15 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # Install ansible
 
-# sudo apt install software-properties-common
-# sudo apt-add-repository --yes --update ppa:ansible/ansible
-# sudo apt install ansible
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-sudo apt install python3-pip -y
-pip3 install --user ansible
-ansible --version
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+# mkdir -p ~/.local/bin
+# echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+# source ~/.bashrc
+# sudo apt install python3-pip -y
+# pip3 install --user ansible
+# ansible --version
 
 type ansible
 type ansible-playbook
