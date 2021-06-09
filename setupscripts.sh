ee7ddd06982e927a6882a5f36547f9f94534166c
VikhilP <vikhilp98@gmail.com>
2021-06-09 14:09:55 +0000

    added ansible install

--- setupscripts.sh
+++ setupscripts.sh
@@ -12,6 +12,7 @@ sudo apt-get update
 sudo apt install curl -y
 curl https://get.docker.com | sudo bash
 
+
 # Docker-compose
 sudo apt update
 sudo apt install -y curl jq
@@ -19,5 +20,11 @@ sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Install ansible
+
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 
 pip3 install -r requirements.txt
