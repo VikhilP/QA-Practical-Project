b50ef67f90306f302b4bd9fc6a9a9df7af68408e
VikhilP <82821513+VikhilP@users.noreply.github.com>
2021-06-10 15:27:57 +0100

    Update app.py

--- service-2/app.py
+++ service-2/app.py
@@ -4,8 +4,7 @@ import random
 app = Flask(__name__)
 
 #positions
-positions = ["QB","HB","FB","WR","T","G","C","TE",
-    "CB","LB","DT","DE","SS","FS","K","KR","P"]
+positions = ["SS","1B","2B","3B","LF","RF","CF","DH","C"]
 
 
 @app.route('/getpositions', methods= ["GET"])
