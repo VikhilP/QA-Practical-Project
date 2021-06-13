d84776a7ab2378b46fc81b3a70aed5d9c32249f8
VikhilP <vikhilp98@gmail.com>
2021-06-13 13:56:18 +0000

    Testing changes

--- service-4/testing/test_service-4.py
+++ service-4/testing/test_service-4.py
@@ -17,53 +17,13 @@ class TestResponse(TestBase):
         #infos = info.json()
         
         self.assertIn("'draft_round': 2",str(info))
-
-        # position = "QB"
-        # pick = 1
-        # senditems = {"dnum": pick, "pos": position}
-        # info = self.client.post(url_for("calculatedraftround"), json=senditems).json
-        # print(info)
-        # print("Testing variables")
-        # #infos = info.json()
+    
+    def test_service4_again(self):
+        position = "SS"
+        pick = 30
+        senditems = {"dnum": pick, "pos": position}
+        info = self.client.post(url_for("calculatedraftround"), json=senditems).json
         
-        # self.assertIn("'draft_round': 1",str(info))
-
-        # position = "QB"
-        # pick = 33
-        # senditems = {"dnum": pick, "pos": position}
-        # info = self.client.post(url_for("calculatedraftround"), json=senditems).json
-        # print(info)
-        # print("Testing variables")
-        # #infos = info.json()
         
-        # self.assertIn("'draft_round': 2",str(info))
-
-        # position = "QB"
-        # pick = 65
-        # senditems = {"dnum": pick, "pos": position}
-        # info = self.client.post(url_for("calculatedraftround"), json=senditems).json
-        # print(info)
-        # print("Testing variables")
-        # #infos = info.json()
-        
-        # self.assertIn("'draft_round': 3",str(info))
-
-        # position = "QB"
-        # pick = 97
-        # senditems = {"dnum": pick, "pos": position}
-        # info = self.client.post(url_for("calculatedraftround"), json=senditems).json
-        # print(info)
-        # print("Testing variables")
-        # #infos = info.json()
-        
-        # self.assertIn("'draft_round': 4",str(info))
-
-        # position = "QB"
-        # pick = 165
-        # senditems = {"dnum": pick, "pos": position}
-        # info = self.client.post(url_for("calculatedraftround"), json=senditems).json
-        # print(info)
-        # print("Testing variables")
-        # #infos = info.json()
-        
-        # self.assertIn("'draft_round': 6",str(info))
+        self.assertIn("'round_pick': 30",str(info))
+       
